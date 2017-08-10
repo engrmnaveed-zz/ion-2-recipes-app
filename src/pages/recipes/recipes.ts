4dc1bab0e665936bcddf240e0eaca11a46c86d96
muhammad-naveed <m.naveed.ch@gmail.com>
2017-08-10 18:38:16 +0500

    displaying the list of newely created recipes, also added and tested edit recipe functionality. added a button to add the ingredients of recipe to shopping list.

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {EditRecipePage} from "../edit-recipe/edit-recipe";
+
+import { EditRecipePage } from "../edit-recipe/edit-recipe";
+import { Recipe } from "../../models/recipe";
+import { RecipesService } from "../../services/recipes";
+import { RecipePage } from "../recipe/recipe";
 
 
 @IonicPage()
@@ -10,13 +14,25 @@ import {EditRecipePage} from "../edit-recipe/edit-recipe";
 })
 export class RecipesPage {
 
+  recipes: Recipe[];
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private recipesService: RecipesService
+  ) {
   }
 
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'New'});
+  }
+
+  ionViewWillEnter() {
+    this.recipes = this.recipesService.getRecipes();
+  }
+
+  onLoadRecipe(recipe: Recipe, index: number) {
+    this.navCtrl.push(RecipePage, {recipe: recipe, index: index});
   }
 
 }
