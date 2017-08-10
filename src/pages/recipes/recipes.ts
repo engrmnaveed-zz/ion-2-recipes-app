6210fe352593b8899f79712905030065def80ee9
muhammad-naveed <m.naveed.ch@gmail.com>
2017-08-10 17:19:57 +0500

    added simple basic UI Form for Adding/Editing Recipe.

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {EditRecipePage} from "../edit-recipe/edit-recipe";
 
-/**
- * Generated class for the RecipesPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +10,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RecipesPage {
 
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RecipesPage');
+  onNewRecipe() {
+    this.navCtrl.push(EditRecipePage, {mode: 'New'});
   }
 
 }
