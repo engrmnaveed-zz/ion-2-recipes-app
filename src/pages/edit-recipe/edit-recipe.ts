4e5dbc0db019ddcca48bd0ed6de97686582e4115
muhammad-naveed <m.naveed.ch@gmail.com>
2017-08-10 17:56:43 +0500

    added form controls to the recipe form, using reactive approach.

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 
 
 @IonicPage()
@@ -11,12 +12,26 @@ export class EditRecipePage implements OnInit{
 
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
   }
 
 }
